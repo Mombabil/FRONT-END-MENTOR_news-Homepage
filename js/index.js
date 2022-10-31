1a1645f0a68b728f57d98ec003990b6a47feebd5
Mombabil <mombabil.7@gmail.com>
2022-10-31 11:53:40 +0100

    front-end Mentor eval

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const menu = document.querySelector(".closeMenu");
+const toggleMenu = document.querySelector(".openClose");
+
+const closeMenu = `<svg width="40" height="17" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z"/><path d="M0 0h40v3H0z"/></g></svg>`;
+
+const openMenu = `<svg width="32" height="31" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z"/><path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z"/></g></svg>`;
+
+toggleMenu.innerHTML = closeMenu;
+
+toggleMenu.addEventListener("click", () => {
+  menu.classList.toggle("openMenu");
+  if (menu.classList == "closeMenu openMenu") {
+    toggleMenu.innerHTML = openMenu;
+  } else {
+    toggleMenu.innerHTML = closeMenu;
+  }
+});
